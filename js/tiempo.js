acb2e714edca2be3831bcd23463094bba6808d61
MoiRouhs <carsrouhs@hotmail.com>
2015-08-21 03:00:54 -0500

    cambia tiempo

--- js/tiempo.js
+++ js/tiempo.js
@@ -5,4 +5,16 @@ function tiem(){
     var ot =tiempo.currentTime;
     console.log(ot);
     
+    if (ot >= 5){
+	slide.className = ("margen1");
+    if(ot >= 68){
+	slide.className = ("margen2");
+    if(ot >= 104){
+	slide.className = ("marco3");
+    }
+    }
+    }else{
+	slide.className = ("margen");
+    }
+    
 }
